811b26fbae46d0ad570f1bdc9323a39649a9e118
tan99tech <tan@99tech.co>
2024-07-08 13:50:29 +0700

    chore: :fire: remove state

--- routes/public.ts
+++ routes/public.ts
@@ -1,13 +1,11 @@
 import { Router, Request, Response, NextFunction } from "express";
 import pug from "pug";
-import state from "../state";
 
 const router = Router();
 
 const home_controller = (req: Request, res: Response, next: NextFunction) => {
   const html = pug.renderFile("./templates/home.pug");
-  res.send(html)
-  res.send(`Current value is: ${state.value}`);
+  res.send(html);
 };
 
 // replace app with router here

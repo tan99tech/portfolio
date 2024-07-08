42680d06476510fb58c9cedcce0ea96869a7134d
tan99tech <tan@99tech.co>
2024-07-08 13:49:09 +0700

    feat:

--- index.ts
+++ index.ts
@@ -1,14 +1,10 @@
 import express from "express";
-import admin_router from "./routes/admin";
 import public_router from "./routes/public";
-import prophecy_router from "./routes/prophecy";
 
 const HTTP_PORT: number = 8181;
 const app: express.Express = express();
 
-app.use("/prophecy", prophecy_router);
-app.use("/admin", admin_router);
-app.use("/public", public_router);
+app.use("/", public_router);
 
 app.listen(HTTP_PORT, () => {
   console.log(`server listening on ${HTTP_PORT}`);
--- routes/public.ts
+++ routes/public.ts
@@ -1,14 +1,17 @@
 import { Router, Request, Response, NextFunction } from "express";
+import pug from "pug";
 import state from "../state";
 
 const router = Router();
 
-const retrieve_controller = (req: Request, res: Response, next: NextFunction) => {
+const home_controller = (req: Request, res: Response, next: NextFunction) => {
+  const html = pug.renderFile("./templates/home.pug");
+  res.send(html)
   res.send(`Current value is: ${state.value}`);
 };
 
 // replace app with router here
 // note the path is also shorter
-router.get("/retrieve", retrieve_controller);
+router.get("/", home_controller);
 
 export default router;
